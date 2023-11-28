76b81ca4e075999738c20aad24c5655e9a3b4aba
Lynn-Hu-D <lingyuhu0106@gmail.com>
2023-11-28 01:18:21 -0800

    REST APIs

---
+++ 8.0rest-api/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -0,0 +1,61 @@
+package com.ltp.contacts.service;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import com.ltp.contacts.pojo.Contact;
+import com.ltp.contacts.repository.ContactRepository;
+
+@Service
+@ConditionalOnProperty(name="server.port", havingValue="8080")
+public class ContactServiceImpl implements ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+    
+
+    public  ContactServiceImpl() {
+        System.out.println("Created the contact service implementation");
+    }
+
+    @Override
+    public Contact getContactById(String id) {
+        return contactRepository.getContact(findIndexById(id));
+    }
+
+    private int findIndexById(String id) {
+        return IntStream.range(0, contactRepository.getContacts().size())
+            .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
+            .findFirst()
+            .orElseThrow();
+    }
+
+    @Override
+    public void saveContact(Contact contact) {
+     contactRepository.saveContact(contact);
+    }
+
+    @Override
+    public void updateContact(String id, Contact contact) {
+        contactRepository.updateContact(findIndexById(id), contact);
+    }
+
+    @Override
+    public void deleteContact(String id) {
+        contactRepository.deleteContact(findIndexById(id));
+    }
+
+    @Override
+    public List<Contact> getContacts() {
+        return contactRepository.getContacts();
+    }
+
+    
+
+   
+
+}
